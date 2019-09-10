c1d5874d45e261c11e8a3450d208d7ed2d9505e2
Mohamed Shiraz <shiraz@youplus.com>
2019-09-10 14:46:50 +0530

    use production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,28 @@
-const path = require("path");
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  mode: 'production',
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "",
-    filename: "rinse.js",
-    libraryTarget: "umd"
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '',
+    filename: 'index.js',
+    libraryTarget: 'umd',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  optimization: {
+    minimizer: [new UglifyJsPlugin()],
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
-      }
-    ]
+        loader: 'babel-loader',
+      },
+    ],
   },
-  resolve: {
-    extensions: [".js", ".jsx"]
-  },
-  mode: "production"
 };
